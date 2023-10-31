72969abba739d8cd8a65e8569e05d2f28abe7f54
Ifeoma Ugwuanyi <ifeoma.r.ugwuanyi@gmail.com>
2023-10-30 22:44:23 -0400

    Updating assign_taxonomy.sh

--- Scripts/assign_taxonomy.sh
+++ Scripts/assign_taxonomy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-##Taxonomy was assigned to sequences using Kaiju v1.82 against the NCBI nr+euk database. The parameters used to assign taxonomy
-##are run mode: greedy, minimum match length: 11, minimum match score: 75, allowed mismatches: 5. Instructions on how to install 
-##and run Kaiju can be found here https://github.com/bioinformatics-centre/kaiju
-#Unzipping and renaming trimmed sequences
+
+#######################################################################################################################
+# Taxonomy was assigned to sequences using Kaiju v1.82 against the NCBI nr+euk database. The parameters used to assign taxonomy
+# are run mode: greedy, minimum match length: 11, minimum match score: 75, allowed mismatches: 5. Instructions on how to install 
+# and run Kaiju can be found here https://github.com/bioinformatics-centre/kaiju
+#######################################################################################################################
+
+#Unzipping and renaming trimmed sequences to names suitable for Kaiju
 cd trimmed_sequences
 gunzip *.gz
 for i in {1..14};
@@ -11,13 +15,15 @@ mv ${i}_forward_paired.fastq ${i}_paired_1.fastq
 mv ${i}_reverse_paired.fastq ${i}_paired_2.fastq
 done
 cd
+
 #Creating Kaiju index
 cd kaiju
 mkdir kaijudb
 cd kaijudb
 kaiju-makedb -s nr_euk
 cd
-#Running Kaiju
+
+# Annotating sequences with Kaiju
 mkdir kaiju_output
 kaiju-multi -z 48 -t kaiju/bin/kaijudb/nodes.dmp kaiju/bin/kaijudb/kaiju_db_nr_euk.fmi -v -e 5 -s 75\
 -i trimmed_sequences/1_paired_1.fastq,trimmed_sequences/2_paired_1.fastq,trimmed_sequences/3_paired_1.fastq,\
@@ -33,7 +39,8 @@ trimmed_sequences/14_paired_2.fastq > kaiju_output/1_taxonomyKaiju.out,kaiju_out
 kaiju_output/3_taxonomyKaiju.out,kaiju_output/4_taxonomyKaiju.out,kaiju_output/5_taxonomyKaiju.out,kaiju_output/6_taxonomyKaiju.out,\
 kaiju_output/7_taxonomyKaiju.out,kaiju_output/8_taxonomyKaiju.out,kaiju_output/9_taxonomyKaiju.out,kaiju_output/10_taxonomyKaiju.out,\
 kaiju_output/11_taxonomyKaiju.out,kaiju_output/12_taxonomyKaiju.out,kaiju_output/13_taxonomyKaiju.out,kaiju_output/14_taxonomyKaiju.out
-#Generating summary taxonomy table from the output above. Unclassified reads were excluded from the total number of reads when calculating percentages.
+
+#Generating summary taxonomy table from the output above. Unclassified reads excluded from the total number of reads when calculating percentages.
 cd kaiju_output
 kaiju2table -t ~/kaiju/bin/kaijudb/nodes.dmp -n ~/kaiju/bin/kaijudb/names.dmp -r species -l superkingdom,phylum,class,order,family,\
 genus,species -e -u -o JS_taxonomytable.tsv 1_taxonomyKaiju.out 2_taxonomyKaiju.out 3_taxonomyKaiju.out 4_taxonomyKaiju.out \
