e817f0b7973222dfdd250464d91f55ba0f85f748
Ifeoma Ugwuanyi <ifeoma.r.ugwuanyi@gmail.com>
2023-11-10 11:22:04 -0500

    Created JS_metagenome_sequence_analysis.r and deleted Sequence_processing_script.sh

---
+++ JS_metagenome_sequence_analysis.r
@@ -0,0 +1,17 @@
+#######################################################################################################################
+#
+# GIVE A BRIEF DESCRIPTION OF THE PROJECT HERE (You can use abstract)
+#
+#######################################################################################################################
+
+
+##load required libraries
+library(dplyr) # Tidying data
+library(tidyverse) # Set of packages for data manipulation and visualization
+library(tidyr) # Data manipulation
+library(readxl) # Importing excel files
+library(phyloseq) # Analysis of microbiome data
+library(vegan) # Community ecology analysis
+library(EcolUtils) # Community ecology analysis
+library(stringr) # String manipulation
+library(ggplot2) # Data Visualization
