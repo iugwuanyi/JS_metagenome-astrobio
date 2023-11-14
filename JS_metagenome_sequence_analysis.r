672ad134127a4b0af8bc062ce7bf1dedb00fe002
Ifeoma Ugwuanyi <ifeoma.r.ugwuanyi@gmail.com>
2023-11-13 22:27:50 -0500

    Updated JS_metagenome_sequence_analysis.r

--- JS_metagenome_sequence_analysis.r
+++ JS_metagenome_sequence_analysis.r
@@ -20,7 +20,9 @@ library(stringr) # String manipulation
 library(ggplot2) # Data Visualization
 
 ####################################################################################################################### 
-# Below are a few functions used for data manipulation and analyses 
+#
+# Below are a few functions used in this script for data manipulation and analyses 
+#
 #######################################################################################################################
 
 #generate clean taxonomy table from kaiju output
@@ -34,3 +36,23 @@ clean_taxatable <- function(taxa_table){
     ungroup() %>% pivot_wider(names_from = Sample, values_from = Reads, values_fill = 0)
 }
 
+#################################################################################################################################################
+#
+# Correlation between temperature and pH measurements taken from sampling points along Jotun active spring (in situ) at three different years
+#
+#################################################################################################################################################
+
+#temperature and pH measurements taken from the spring at 3 different years (2007, 2009, and 2011) 
+Jotun_temp <- c(21.00, 18.90, 12.80, 22.10, 22.00, 21.40, 21.50, 21.90, 22.10, 22.00, 20.80)
+Jotun_pH <- c(6.69, 6.79, 7.86, 6.55, 6.58, 6.25, 6.54, 6.60, 6.59, 6.63, 6.86)
+
+#linear regression analysis between temperature and pH
+temp_ph.lm <- lm(Jotun_temp ~ Jotun_pH)
+summary(temp_ph.lm)
+
+# Import taxonomy table and prepare table for downstream analyses
+Taxonomy_table <- read.table("Data_files/JS_taxonomytable.tsv", header = TRUE, sep = "\t")
+Taxonomy_table <- clean_taxatable(Taxonomy_table) %>% 
+  dplyr::rename(c("A-S1" = "1", "A-S2" = "2", "A-1" = "3", "A-2" = "4", "A-3" = "5", "A-4" = "6",
+                  "A-5" = "7", "R-S" = "8", "R-1" = "9", "R-2" = "10", "R-3" = "11", 
+                  "R-4_a" = "12", "R-4_b" = "13", "R-5" = "14"))
